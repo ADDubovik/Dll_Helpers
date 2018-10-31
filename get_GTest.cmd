bd6d51e01fe35191594fa36928088954a5c9c9c5
Alexander Dubovik <dubovik_a_d@mail.ru>
2018-10-31 18:00:10 +0300

    Initial approach, does not build

---
+++ get_GTest.cmd
@@ -0,0 +1,2 @@
+cd 3rdparty
+git clone https://github.com/google/googletest.git
---
+++ src/Tests/main.h
@@ -0,0 +1,7 @@
+﻿
+
+int main(int ac, char* av[])
+{
+	testing::InitGoogleTest(&ac, av);
+	return RUN_ALL_TESTS();
+}
