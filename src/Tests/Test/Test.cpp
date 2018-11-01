2848e3deb351d94488a07843f63282d7892ce2ab
Alexander Dubovik <dubovik_a_d@mail.ru>
2018-11-01 18:36:15 +0300

    DllTest advance

--- src/Tests/Test/Test.cpp
+++ src/Tests/Test/Test.cpp
@@ -3,7 +3,7 @@
 #include "DllHelpers.h"
 
 
-TEST(DllTest, Test01)
+TEST(DllTest, LibrarySuccessLoadTest)
 {
 		try
 		{
@@ -13,6 +13,78 @@ TEST(DllTest, Test01)
 		{
 			// Should not execute this
 			EXPECT_TRUE(false);
+		}
+}
+
+
+TEST(DllTest, LibraryUnsuccessLoadTest)
+{
+		try
+		{
+			DllHelpers::Library lib("sjhggkjytrjqweoirtenslakjfkgrehjksfakldjhfgkrkdhgkdfsfaewfsvklrhskdjf.dll");
+			// Should not execute this because of exception
+			EXPECT_TRUE(false);
+		}
+		catch (std::exception &e)
+		{
+			// Should execute this
+			EXPECT_TRUE(true);
+			EXPECT_EQ(std::string(e.what()), "Can't load library sjhggkjytrjqweoirtenslakjfkgrehjksfakldjhfgkrkdhgkdfsfaewfsvklrhskdjf.dll");
+		}
+}
+
+
+TEST(DllTest, FunctionSuccessTest01)
+{
+		try
+		{
+			DllHelpers::Library lib("TestDll.dll");
+			DllHelpers::Function<DllHelpers::__cdecl__, double, double, double>   sum_cdecl  (lib, "sum_cdecl");
+			DllHelpers::Function<DllHelpers::__stdcall__, double, double, double> sum_stdcall(lib, "sum_stdcall");
+
+			EXPECT_EQ(  sum_cdecl(3.0, 5.0), 3.0+5.0);
+			EXPECT_EQ(sum_stdcall(3.0, 5.0), 3.0+5.0);
+		}
+		catch (std::exception)
+		{
+			// Should not execute this
+			EXPECT_TRUE(false);
+		}
+}
+
+
+TEST(DllTest, FunctionUnsuccessTest01)
+{
+		try
+		{
+			DllHelpers::Library lib("TestDll.dll");
+			DllHelpers::Function<DllHelpers::__cdecl__, double, double, double>   sum_cdecl  (lib, "sum_lkdjgklfjslgjfd");
+			// Should not execute this because of exception
+			EXPECT_TRUE(false);
+		}
+		catch (std::exception &e)
+		{
+			// Should execute this
+			EXPECT_TRUE(true);
+			EXPECT_EQ(std::string(e.what()), "Can't get proc address for sum_lkdjgklfjslgjfd");
+		}
+}
+
+
+TEST(DllTest, FunctionUnsuccessTest02)
+{
+		try
+		{
+			DllHelpers::Library lib("TestDll.dll");
+			DllHelpers::Function<DllHelpers::__stdcall__, double, double, double> sum_stdcall(lib, "sum_jsdgtiwesufd");
+			// Should not execute this because of exception
+			EXPECT_TRUE(false);
+		}
+		catch (std::exception &e)
+		{
+			// Should execute this
+			EXPECT_TRUE(true);
+			EXPECT_EQ(std::string(e.what()), "Can't get proc address for sum_jsdgtiwesufd");
 		}
 }
 
