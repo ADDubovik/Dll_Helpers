fe82858c6a4668ef67b5801dc6cc25b83bb21a1c
Alexander Dubovik <dubovik_a_d@mail.ru>
2018-11-01 17:02:13 +0300

    DllHelpers.h major advance

--- src/Helpers/DllHelpers.h
+++ src/Helpers/DllHelpers.h
@@ -5,43 +5,73 @@
 #include <string>
 
 
-// Forward declaration
-template<typename ReturnType, typename ... Args> class Function;
-
-// RAII for .dll handle
-class Library
+namespace DllHelpers
 {
-	private:
-		HMODULE m_handle = NULL;
-		template<typename, typename ...> friend class Function;
-	public:
-		Library(const std::string &name)
-		{
-				if ( !(m_handle = LoadLibraryA(name.c_str())) )
-					throw std::runtime_error( std::string("Can't load library") + name );
-		}
-		~Library()
-		{
-				if ( m_handle )
-					FreeLibrary(m_handle);
-		}
-};
+	enum CallingConventions {__stdcall__, __cdecl__};
 
 
-// Variadic template representing a function imported from .dll
-template<typename ReturnType, typename ... Args> class Function
-{
-	private:
-		const Library &m_lib;
-		using Fn = ReturnType (__stdcall *)(Args ...);
-		Fn m_func = NULL;
-	public:
-		Function(const Library &lib, const std::string &name) : m_lib(lib)
-		{
-				if ( !(m_func = (Fn)GetProcAddress(m_lib.m_handle, name.c_str())) )
-					throw std::runtime_error( std::string("Can't get proc address for ") + name );
-		}
-		~Function() {}
+	// Forward declaration
+	template<CallingConventions, typename ReturnType, typename ... Args> class Function;
+	
 
-		ReturnType operator()(Args ... args) const { return m_func(args ...); }
-};
+	// RAII for .dll handle
+	class Library
+	{
+		private:
+			HMODULE m_handle = NULL;
+			template<CallingConventions, typename, typename ...> friend class Function;
+		public:
+			Library(const std::string &name)
+			{
+					if ( !(m_handle = LoadLibraryA(name.c_str())) )
+						throw std::runtime_error( std::string("Can't load library") + name );
+			}
+			~Library()
+			{
+					if ( m_handle )
+						FreeLibrary(m_handle);
+			}
+	};
+
+
+	namespace Implementation
+	{
+		template<CallingConventions convention, typename ReturnType, typename ... Args> struct FnTypeProvider
+		{
+			// Forward declaration.
+			class Fn;
+		};
+
+		template<typename ReturnType, typename ... Args> struct FnTypeProvider<CallingConventions::__cdecl__, ReturnType, Args ...>
+		{
+			// Function type with calling convention "cdecl"
+			using Fn = ReturnType (__cdecl *)(Args ...);
+		};
+
+		template<typename ReturnType, typename ... Args> struct FnTypeProvider<CallingConventions::__stdcall__, ReturnType, Args ...>
+		{
+			// Function type with calling convention "stdcall"
+			using Fn = ReturnType (__stdcall *)(Args ...);
+		};
+	} // namespace Implementation
+
+
+	// Variadic template representing a function imported from .dll
+	template<CallingConventions convention, typename ReturnType, typename ... Args> class Function
+	{
+		private:
+			const Library &m_lib;
+			//using Fn = ReturnType (__stdcall *)(Args ...);
+			using Fn = FnTypeProvider<convention, ReturnType, Args ...>::Fn;
+			Fn m_func = NULL;
+		public:
+			Function(const Library &lib, const std::string &name) : m_lib(lib)
+			{
+					if ( !(m_func = (Fn)GetProcAddress(m_lib.m_handle, name.c_str())) )
+						throw std::runtime_error( std::string("Can't get proc address for ") + name );
+			}
+			~Function() {}
+
+			ReturnType operator()(Args ... args) const { return m_func(args ...); }
+	};
+} // namespace DllHelpers
