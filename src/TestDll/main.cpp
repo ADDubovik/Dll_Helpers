8c1eda216a31af40e5ccbe36f4d81ed8b90ce8ce
Alexander Dubovik <dubovik_a_d@mail.ru>
2018-11-01 11:24:55 +0300

    Test DLL advance

--- src/TestDll/main.cpp
+++ src/TestDll/main.cpp
@@ -1,5 +1,22 @@
-
 #include <windows.h>
+
+
+#define PREFIX  extern "C" __declspec(dllexport)
+// Some magic (MSVC-specific?) to get function name undecorated without adding a .def-file
+#define EXPORT comment(linker, "/EXPORT:" __FUNCTION__ "=" __FUNCDNAME__)
+
+
+PREFIX double __stdcall sum_stdcall(const double a, const double b)
+{
+#pragma EXPORT
+	return a + b;
+}
+
+
+PREFIX double __cdecl sum_cdecl(const double a, const double b)
+{
+	return a + b;
+}
 
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
