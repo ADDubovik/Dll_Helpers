2af9df13ff37536d5f946095d06bf786a6026f47
Alexander Dubovik <dubovik_a_d@mail.ru>
2018-11-01 17:49:19 +0300

    DllTest started

--- src/Tests/Test/Test.cpp
+++ src/Tests/Test/Test.cpp
@@ -1,6 +1,20 @@
 #include <gtest/gtest.h>
 
+#include "DllHelpers.h"
 
+
+TEST(DllTest, Test01)
+{
+		try
+		{
+			DllHelpers::Library lib("TestDll.dll");
+		}
+		catch (std::exception)
+		{
+			// Should not execute this
+			EXPECT_TRUE(false);
+		}
+}
 
 
 
